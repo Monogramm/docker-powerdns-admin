946997548b0cbcaad175e6be8bbb5740a9a665a0
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-01-06 11:10:25 +0100

    :art: Remove unneeded env var
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/master/test/docker_test.sh
+++ images/master/test/docker_test.sh
@@ -6,12 +6,12 @@ echo "Waiting to ensure everything is fully ready for the tests..."
 sleep 90
 
 echo "Checking main containers are reachable..."
-if ! ping -c 10 -q "${PDNS_HOST}" ; then
+if ! ping -c 10 -q 'pdns-server' ; then
     echo 'PowerDNS Server container is not responding!'
     # TODO Display logs to help bug fixing
     #echo 'Check the following logs for details:'
     #tail -n 100 logs/*.log
-    #exit 1
+    exit 1
 fi
 
 if ! ping -c 10 -q 'powerdns-admin' ; then
