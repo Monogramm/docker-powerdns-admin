7f8129f9b611544d35983fb29b776f67cc2a0bc8
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-01-06 05:40:52 +0100

    :bug: Add app context for scripts
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- template/init_admin.py
+++ template/init_admin.py
@@ -2,23 +2,28 @@
 
 import os
 
+from powerdnsadmin import create_app
 from powerdnsadmin.models.user import User
 
+app = create_app()
+app.logger.setLevel(logging.INFO)
 
-# Admin user
-ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
-ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
-ADMIN_FIRSTNAME = os.environ.get('ADMIN_FIRSTNAME')
-ADMIN_LASTNAME = os.environ.get('ADMIN_LASTNAME')
-ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
-admin_user = User(username=ADMIN_USERNAME,
-    plain_text_password=ADMIN_PASSWORD,
-    firstname=ADMIN_FIRSTNAME,
-    lastname=ADMIN_LASTNAME,
-    email=ADMIN_EMAIL)
 
-result = admin_user.create_local_user()
-if result and result['status']:
-    print('Admin user created: ' + ADMIN_USERNAME)
-else:
-    print('Could not create admin user: ' + result['msg'])
+with app.app_context():
+    # Admin user
+    ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
+    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
+    ADMIN_FIRSTNAME = os.environ.get('ADMIN_FIRSTNAME')
+    ADMIN_LASTNAME = os.environ.get('ADMIN_LASTNAME')
+    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
+    admin_user = User(username=ADMIN_USERNAME,
+        plain_text_password=ADMIN_PASSWORD,
+        firstname=ADMIN_FIRSTNAME,
+        lastname=ADMIN_LASTNAME,
+        email=ADMIN_EMAIL)
+
+    result = admin_user.create_local_user()
+    if result and result['status']:
+        print('Admin user created: ' + ADMIN_USERNAME)
+    else:
+        print('Could not create admin user: ' + result['msg'])
