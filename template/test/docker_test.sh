ccd07e2b768c0135afec4c80987ff9ee2bac38a2
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-01-06 01:39:21 +0100

    :art: Add quotes around docker tests
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- template/test/docker_test.sh
+++ template/test/docker_test.sh
@@ -6,20 +6,20 @@ echo "Waiting to ensure everything is fully ready for the tests..."
 sleep 60
 
 echo "Checking main containers are reachable..."
-if ! ping -c 10 -q ${PDNS_HOST} ; then
+if ! ping -c 10 -q "${PDNS_HOST}" ; then
     echo 'PowerDNS Server container is not responding!'
     # TODO Display logs to help bug fixing
     #echo 'Check the following logs for details:'
     #tail -n 100 logs/*.log
-    exit 2
+    exit 1
 fi
 
-if ! ping -c 10 -q powerdns-admin ; then
+if ! ping -c 10 -q 'powerdns-admin' ; then
     echo 'PowerDNS-Admin container is not responding!'
     # TODO Display logs to help bug fixing
     #echo 'Check the following logs for details:'
     #tail -n 100 logs/*.log
-    exit 4
+    exit 2
 fi
 
 # XXX Add your own tests
